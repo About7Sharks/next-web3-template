697da7cd3e2ccf84934f47116fbb483537ae6627
Zach <zacarlin@gmail.com>
2022-02-21 23:06:19 -0500

    Added support for ens reverse lookup. If a wallet has properly set up their ens resolver show the readable name instead of the wallet address.

--- components/wallet.js
+++ components/wallet.js
@@ -3,22 +3,34 @@ import { injected} from '../Connector/connectors'
 import { useWeb3React } from '@web3-react/core'
 import {Button } from 'antd'
 import Styles from  '../styles/Wallet.module.css'
-
+import { useEffect,useState } from 'react'
+import {ethers} from 'ethers'
 export default function Wallet (){
-  const {active,account,library,connector,activate,deactivate} = useWeb3React()
+  const {active,account,activate,deactivate} = useWeb3React()
+  const [ens, setEns] = useState(null);
   async function connect(){
-    try{
      await activate(injected)
-    }catch(e){
-     console.log(e)
-    }
   }
+  useEffect(()=>{
+    async function lookupENS(){
+      const provider = new ethers.providers.Web3Provider(ethereum)
+      const ens = await provider.lookupAddress(account);
+      if(ens){
+        setEns(ens)
+      }else{
+        console.log('Think about setting your ens resolver records https://docs.ens.domains/contract-api-reference/ens')
+      }
+    }
+    // if signed in check for ens resolver
+  if(active){ lookupENS() }},[account, active])
+
   async function disconnect(){
       deactivate(injected)
   }
   return (
     <Button className={Styles.wallet} size='large' onClick={active?disconnect:connect} >
-      {active?account.substring(0,5)+'...':'Sign-in'} 
+    {/* if ens has been set display it; if active display wallet if not active show sign in */}
+      {ens?ens:active?account.substring(0,5)+'...':'Sign-in'} 
     </Button>
   )
 }
